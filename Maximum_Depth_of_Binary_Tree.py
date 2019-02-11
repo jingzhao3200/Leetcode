58dc0718e631b46f941f86dcd6b79556aad227a0
jingzhao3200 <jingzhao3200@users.noreply.github.com>
2019-02-10 23:48:42 -0500

    add a leetcode code

---
+++ Maximum_Depth_of_Binary_Tree.py
@@ -0,0 +1,26 @@
+# Leetcode
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if root == None:
+            return 0
+        else:
+            left_depth = self.maxDepth(root.left)
+            right_depth = self.maxDepth(root.right)
+            max_depth = max(left_depth, right_depth) +1
+            
+        return max_depth
+    
+    
+        
