7a178c42cee94238470515584273501a96244a0b
jingzhao3200 <ms.zj@outlook.com>
2019-03-23 16:30:28 -0400

    Update 253. Meeting Rooms II.py

--- 253. Meeting Rooms II.py
+++ 253. Meeting Rooms II.py
@@ -10,16 +10,61 @@ class Solution(object):
         :type intervals: List[Interval]
         :rtype: int
         """
-        intervals.sort(key = lambda x: x.start)
-        heap = []
+        intervals.sort( key = lambda x: x.start)
         
+        heap = []
         for i in intervals:
-            if heap and (heap[0] <= i.start):
-                #  the direct implementation would be keep a heap to store every room's latest event ending time.
-                heapq.heapreplace(heap, i.end)
+            if heap and i.start>=heap[0]:
+                heap[0] = i.end
+                # heapq.heapreplace(heap, i.end)
             else:
-                heapq.heappush(heap, i.end)
-            
+                heap.append(i.end)
+                # heapq.heappush(heap, i.end)
+            heap.sort()
         return len(heap)
+        
+        
+#         intervals.sort( key = lambda x: x.start)
+        
+#         heap = []
+#         for i in intervals:
+            
+#             if heap and i.start>=heap[0]:  #can use the same room 
+#                 print('i.start:',i.start)
+#                 print('heap 0:', heap[0])
+#                 print(heap)
+#                 heapq.heapreplace(heap, i.end) 
+#             else:    #have a new room
+#                 heapq.heappush(heap, i.end) 
+        
+#         return len(heap)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         intervals.sort(key = lambda x: x.start)
+#         heap = []
+        
+#         for i in intervals:
+#             if heap and (heap[0] <= i.start):
+#                 #  the direct implementation would be keep a heap to store every room's latest event ending time.
+#                 heapq.heapreplace(heap, i.end)
+#             else:
+#                 heapq.heappush(heap, i.end)
+            
+#         return len(heap)
 
         
+    
+    
+    
+                    
+            
