39d6671d5c67fa5921e32446c27d00c3da23294f
jingzhao3200 <jingzhao3200@users.noreply.github.com>
2019-07-08 00:03:27 -0700

    add another solution of 3

---
+++ 3. Longest_Substring_Without_Repeating_Characters_SlidingWindow.cpp
@@ -0,0 +1,73 @@
+//
+// 3. Longest Substring Without Repeating Characters
+// Given a string, find the length of the longest substring without repeating characters.
+//
+
+#include <iostream>    // std::cout
+#include <algorithm>  //std::max
+#include <set>
+#include <string>
+#include <unordered_set>
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(std::string s) {
+        /* brutal force */
+        // int n = s.length();
+        // int ans = 0;
+        // for (int i=0; i<n; i++){
+        //     for (int j=i+1; j<=n; j++){
+        //         if (allUnique(s, i, j)){
+        //             ans = std::max(ans, j-i);
+        //         }
+        //     }
+        // }
+
+        /* sliding window */
+        int n = s.length();
+        int ans = 0, i = 0, j = 0;
+        std::unordered_set <char> charSet;
+        while (i<n && j<n){
+            //try to extend the range[i, j]
+            if (charSet.find(s[j]) == charSet.end()){ // not in set
+                charSet.insert(s[j]);
+                j++;
+                ans = std::max(ans, j-i);
+            }
+            else{
+                charSet.erase(s[i]);
+                i++;
+            }
+        }
+        return ans;
+    }
+
+    // bool allUnique(std::string s, int start, int end){
+    //     std::set<char>unique_set;
+    //     for (int i=start; i<end; i++){
+    //         auto search = unique_set.find(s[i]);
+    //         if (search != unique_set.end()){
+    //             return false;
+    //         }
+    //         else{
+    //             unique_set.insert(s[i]);
+    //         }
+    //     }
+    //     return true;
+    // }
+};
+
+
+int main(){
+    std::string s = "abcddef";
+    Solution solution;
+    int res = solution.lengthOfLongestSubstring(s);
+    std::string out = std::to_string(res);
+    std::cout << out << std::endl;
+
+    return  0;
+}
+
+
+
+
