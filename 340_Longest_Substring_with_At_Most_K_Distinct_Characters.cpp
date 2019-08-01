240d1bd8b3eab6d5912400980accff85d652e3b7
jingzhao3200 <jingzhao3200@users.noreply.github.com>
2019-07-31 22:24:31 -0700

    add a new file

---
+++ 340_Longest_Substring_with_At_Most_K_Distinct_Characters.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+
+class Solution {
+public:
+    int lengthOfLongestSubstringKDistinct(string s, int k) {
+        return k;
+    }
+};
+
+int main(){
+	cout << "test";
+	return 0;
+}
